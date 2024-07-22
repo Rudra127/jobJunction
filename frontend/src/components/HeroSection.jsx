9cbcc4145b9a6748881f3744bf22a5828decf710
Rudra127 <prajapatirudra127@gmail.com>
2024-07-22 15:43:46 +0530

    ok

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -26,7 +26,6 @@ const HeroSection = () => {
                     <h1 className='text-3xl md:text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
                 </div>
                 <div>
-                    <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus adipisci cupiditate cum.<br /> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 </div>
                 <div className='flex w-full max-w-md lg:max-w-lg shadow-lg border pl-3 border-gray-200 rounded-full items-center gap-4 mx-auto'>
                     <input
